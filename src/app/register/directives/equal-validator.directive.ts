fb1e5443164b990d12779566ba99441a78f15155
suzichenapk@gmail.com <suzichenapk@gmail.com>
2017-11-23 20:53:59 +0800

    Add comments and routerLinkActive

--- src/app/register/directives/equal-validator.directive.ts
+++ src/app/register/directives/equal-validator.directive.ts
@@ -21,11 +21,13 @@ export class EqualValidator implements Validator {
         let targetControl = control.root.get(this.validateEqual);
         // 值不相等
         if (targetControl && selfValue !== targetControl.value ) {
+            // 如果输入框是"确认密码",直接返回错误信息
             if(!this.reverse){
                 return {
                     // validateEqual: {description: '两次输入的密码不一致。'}
                     validateEqual: false
                 }
+            // 如果输入框不是"确认密码"，给目标值输入框添加错误信息
             }else{
                 targetControl.setErrors({
                     validateEqual: false
